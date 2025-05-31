4296bc582f3f2c88745d10dcd48bf595c068ea10
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-05-31 23:15:41 +0300

    include api router to main app

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -1,9 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
 
+from api import router as api_router
 from core.config import settings
 
 app = FastAPI()
+app.include_router(
+    router=api_router,
+)
+
+
 @app.get("/")
 async def index():
     return {"message": "Hello Index!"}
