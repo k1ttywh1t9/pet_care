7abf6883c0eba15477ce506dbb143dc10288c9b5
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-05-31 22:52:10 +0300

    create main page endpoint

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -4,6 +4,10 @@ from fastapi import FastAPI
 from core.config import settings
 
 app = FastAPI()
+@app.get("/")
+async def index():
+    return {"message": "Hello Index!"}
+
 
 if __name__ == "__main__":
     uvicorn.run(
