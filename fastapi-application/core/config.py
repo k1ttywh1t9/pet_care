5b1748b690d416773b5c3bc079ca81b7d66492e7
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-01 16:44:09 +0300

    decompose config

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, PostgresDsn
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class DatabaseConfig(BaseModel):
@@ -8,6 +8,15 @@ class DatabaseConfig(BaseModel):
     echo_pool: bool = False
     pool_size: int = 50
     max_overflow: int = 10
+
+
+class RunConfig(BaseModel):
+    host: str = "0.0.0.0"
+    port: int = 8000
+
+
+class ApiConfig(BaseModel):
+    api_prefix: str = "/api"
 
 
 class Settings(BaseSettings):
