5c92adddeaea8fdc09a97c2a6f8f4064d9f1cfed
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-01 17:04:23 +0300

    add lifespan + db_helper dispose to main app

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -1,23 +1,34 @@
+from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 
 from api import router as api_router
 from core.config import settings
+from core.models import db_helper
 
-app = FastAPI()
-app.include_router(
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # on startup
+    yield
+    # on shutdown
+    await db_helper.dispose()
+
+
+main_app = FastAPI()
+main_app.include_router(
     router=api_router,
 )
 
 
-@app.get("/")
+@main_app.get("/")
 async def index():
     return {"message": "Hello Index!"}
 
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app",
+        "main:main_app",
         host=settings.host,
         port=settings.port,
         reload=True,
