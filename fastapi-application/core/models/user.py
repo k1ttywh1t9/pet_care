29b0a8a14e5fd58076e6edac0d078d3cfda8d65a
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-02 15:09:13 +0300

    add user get db method

--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,5 +1,6 @@
-from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from sqlalchemy import Integer
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
@@ -7,3 +8,13 @@ from .base import Base
 
 class User(SQLAlchemyBaseUserTable[int], Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+
+    @classmethod
+    async def get_db(
+        cls,
+        session: AsyncSession,
+    ):
+        yield SQLAlchemyUserDatabase(
+            session=session,
+            user_table=cls,
+        )
