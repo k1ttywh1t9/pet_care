09f26b2198088145378f5bb71ca34cdbc9d8801d
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-03 23:14:39 +0300

    add register route

--- fastapi-application/api/api_v1/auth.py
+++ fastapi-application/api/api_v1/auth.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from api.api_v1.schemas.user import UserRead, UserCreate
 from api.dependencies.authentication.backend import authentication_backend
 from api.api_v1.users_routers_helper import fastapi_users
 from core.config import settings
@@ -9,8 +10,20 @@ router = APIRouter(
     tags=["Auth"],
 )
 
+
+# login
+# logout
 router.include_router(
     router=fastapi_users.get_auth_router(
         backend=authentication_backend,
+    )
+)
+
+
+# register
+router.include_router(
+    router=fastapi_users.get_register_router(
+        user_schema=UserRead,
+        user_create_schema=UserCreate,
     )
 )
