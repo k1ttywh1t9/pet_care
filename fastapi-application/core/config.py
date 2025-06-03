f425ef4018fc6724227cb39ddba6bc53c002bac5
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-03 22:16:33 +0300

    create auth router + config

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -19,8 +19,11 @@ class RunConfig(BaseModel):
     host: str = "0.0.0.0"
     port: int = 8000
 
+
 class ApiV1Config(BaseModel):
     prefix: str = "/v1"
+    auth: str = "/auth"
+
 
 class ApiConfig(BaseModel):
     prefix: str = "/api"
