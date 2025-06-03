c648d37c09cc4ffb5ce068f3882c7b5631072a6e
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-03 23:14:11 +0300

    register api v1 router

--- fastapi-application/api/__init__.py
+++ fastapi-application/api/__init__.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter
 
 from core.config import settings
+from .api_v1 import router as v1_router
 
 router = APIRouter(
     prefix=settings.api.prefix,
+)
+
+router.include_router(
+    router=v1_router,
 )
