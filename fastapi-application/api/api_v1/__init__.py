4f2fec13c0344f27055a052d6b122a16e9ef96a1
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-05 09:34:00 +0300

    fixed imports in api v1 after refactoring architecture

--- fastapi-application/api/api_v1/__init__.py
+++ fastapi-application/api/api_v1/__init__.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
 from core.config import settings
-from .auth import router as auth_router
-from .users import router as users_router
-from .pets import router as pets_router
+from api.api_v1.users.auth import router as auth_router
+from api.api_v1.users.views import router as users_router
+from api.api_v1.pets.views import router as pets_router
 
 router = APIRouter(
     prefix=settings.api.v1.prefix,
