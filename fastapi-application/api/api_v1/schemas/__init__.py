b6798703778a886e23890315502c527ed83d301f
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-05 09:11:45 +0300

    register pet schemas

--- fastapi-application/api/api_v1/schemas/__init__.py
+++ fastapi-application/api/api_v1/schemas/__init__.py
@@ -2,6 +2,10 @@ __all__ = (
     "UserRead",
     "UserCreate",
     "UserUpdate",
+    "PetCreate",
+    "PetRead",
+    "PetUpdate",
 )
 
 from .user import UserRead, UserCreate, UserUpdate
+from .pet import PetRead, PetCreate, PetUpdate
