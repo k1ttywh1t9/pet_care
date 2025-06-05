3278ec82aef17060e5645396c838d3c7558a3382
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-05 09:04:00 +0300

    create pet schemas

---
+++ fastapi-application/api/api_v1/schemas/pet.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+
+from core.types import UserIdType
+
+
+class PetBase(BaseModel):
+    name: str
+    owner_id: UserIdType
+
+
+class PetRead(PetBase):
+    id: int = 0
+
+
+class PetCreate(PetBase):
+    pass
+
+
+class PetUpdate(PetBase):
+    pass
