05a48b28ac246ca685b9a85113f4443f121a8f0c
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-05 08:03:03 +0300

    create pets user relationship

--- fastapi-application/core/models/pet.py
+++ fastapi-application/core/models/pet.py
@@ -1,10 +1,16 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import ForeignKey, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
 
 from .mixins import IdIntPkMixin
 from core.types import UserIdType
+
+
+if TYPE_CHECKING:
+    from .user import User
 
 
 class Pet(IdIntPkMixin, Base):
@@ -13,3 +19,5 @@ class Pet(IdIntPkMixin, Base):
     owner_id: Mapped[UserIdType] = mapped_column(
         ForeignKey("user.id"),
     )
+
+    owner: Mapped["User"] = relationship(back_populates="pets")
