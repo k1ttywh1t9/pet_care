62bf98a32879050fa4cdd74e5c3a88832c00eace
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-05 07:59:35 +0300

    create user pets relationship

--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,12 +1,19 @@
+from typing import TYPE_CHECKING
+
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Mapped, relationship
 
 from .base import Base
 from .mixins.id_int_pk import IdIntPkMixin
 from core.types import UserIdType
 
+if TYPE_CHECKING:
+    from .pet import Pet
+
 
 class User(IdIntPkMixin, SQLAlchemyBaseUserTable[UserIdType], Base):
+    pets: Mapped[list["Pet"]] = relationship()
 
     @classmethod
     def get_db(
