43185f4db6032dd015e52be8f767e2cf1c4836b9
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-05 22:43:43 +0300

    register users + pets routers

--- fastapi-application/api/api_v1/__init__.py
+++ fastapi-application/api/api_v1/__init__.py
@@ -1,22 +1,18 @@
 from fastapi import APIRouter
 
 from core.config import settings
-from api.api_v1.users.auth import router as auth_router
-from api.api_v1.users.views import router as users_router
-from api.api_v1.pets.views import router as pets_router
+
+from .users import router as users_router
+from .pets import router as pets_router
 
 router = APIRouter(
     prefix=settings.api.v1.prefix,
 )
 
 router.include_router(
-    auth_router,
+    router=users_router,
 )
 
 router.include_router(
-    pets_router,
-)
-
-router.include_router(
-    users_router,
+    router=pets_router,
 )
