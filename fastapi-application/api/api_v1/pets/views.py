6e683b104ef210d5c6bc6ed2bf75bb41a44f770d
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-05 22:42:52 +0300

    add create + read pets views

--- fastapi-application/api/api_v1/pets/views.py
+++ fastapi-application/api/api_v1/pets/views.py
@@ -1,6 +1,11 @@
-from fastapi import APIRouter
+from typing import Annotated
 
-from .schemas import PetCreate, PetRead
+from fastapi import APIRouter, HTTPException, status, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import db_helper
+from . import crud
+from .schemas import PetRead, PetCreate
 
 router = APIRouter(
     prefix="/pets",
@@ -8,12 +13,39 @@ router = APIRouter(
 )
 
 
-@router.get("/{id}")
-async def read_pet(id):
-    pet = PetRead(id=id, name="Susie", owner_id=1)
-    return {"pet": pet}
+@router.get("", response_model=list[PetRead])
+async def get_pets(
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.scoped_session_dependency),
+    ],
+):
+    return await crud.read_pets(session=session)
 
 
-@router.post("")
-async def create_pet(pet: PetCreate):
-    return {"pet_created": PetRead(**pet.model_dump())}
+@router.get("/{pet_id}", response_model=PetRead)
+async def get_pet(
+    pet_id,
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.scoped_session_dependency),
+    ],
+):
+    pet = await crud.read_pet(session=session, pet_id=pet_id)
+    if pet is not None:
+        return pet
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Product {pet_id} not found",
+    )
+
+
+@router.post("", response_model=PetRead)
+async def create_pet(
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.scoped_session_dependency),
+    ],
+    pet_create: PetCreate,
+):
+    return await crud.create_pet(session=session, pet_create=pet_create)
