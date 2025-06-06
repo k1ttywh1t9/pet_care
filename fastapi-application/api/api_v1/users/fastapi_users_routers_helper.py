f5e456121f449b6508c1b5d20e51fc961902cc2d
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-06 08:24:05 +0300

    added current active user dependency

--- fastapi-application/api/api_v1/users/fastapi_users_routers_helper.py
+++ fastapi-application/api/api_v1/users/fastapi_users_routers_helper.py
@@ -1,11 +1,13 @@
 from fastapi_users import FastAPIUsers
 
-from core.models import User
-from core.types import UserIdType
 from api.dependencies.authentication.backend import authentication_backend
 from api.dependencies.authentication.user_manager import get_user_manager
+from core.models import User
+from core.types import UserIdType
 
 fastapi_users = FastAPIUsers[User, UserIdType](
     get_user_manager,
     [authentication_backend],
 )
+
+current_active_user: User = fastapi_users.current_user(active=True)
