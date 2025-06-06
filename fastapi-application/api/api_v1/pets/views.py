ab9c37be6473c18e133b594e0b6e65daaaccee5d
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-06 08:30:26 +0300

    create crud views w/ pets owners access rights

--- fastapi-application/api/api_v1/pets/views.py
+++ fastapi-application/api/api_v1/pets/views.py
@@ -3,27 +3,38 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.models import db_helper, Pet
+from core.models import db_helper, Pet, User
 from . import crud
 from .dependencies import pet_by_id
 from .schemas import PetRead, PetCreate, PetUpdate
+from ..users.fastapi_users_routers_helper import current_active_user
 
 router = APIRouter(
     tags=["Pets"],
 )
 
 
-@router.get("/", response_model=list[PetRead])
+@router.get(
+    "/",
+    response_model=list[PetRead],
+)
 async def get_pets(
+    user: Annotated[User, Depends(current_active_user)],
     session: Annotated[
         AsyncSession,
         Depends(db_helper.scoped_session_dependency),
     ],
 ):
-    return await crud.read_pets(session=session)
+    return await crud.read_pets(
+        session=session,
+        owner_id=user.id,
+    )
 
 
-@router.get("/{pet_id}", response_model=PetRead)
+@router.get(
+    "/{pet_id}",
+    response_model=PetRead,
+)
 async def get_pet(
     pet: Annotated[
         Pet,
@@ -39,16 +50,24 @@ async def get_pet(
     status_code=status.HTTP_201_CREATED,
 )
 async def create_pet(
+    user: Annotated[User, Depends(current_active_user)],
     session: Annotated[
         AsyncSession,
         Depends(db_helper.scoped_session_dependency),
     ],
     pet_create: PetCreate,
 ):
-    return await crud.create_pet(session=session, pet_create=pet_create)
+    return await crud.create_pet(
+        session=session,
+        pet_create=pet_create,
+        owner_id=user.id,
+    )
 
 
-@router.patch("/{pet_id}", response_model=PetRead)
+@router.patch(
+    "/{pet_id}",
+    response_model=PetRead,
+)
 async def update_pet(
     pet_update: PetUpdate,
     pet: Annotated[
