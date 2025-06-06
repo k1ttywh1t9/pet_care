af7eda516436ad0b0367ff921b2611a2748fc92e
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-06 03:39:53 +0300

    create views for pets crud

--- fastapi-application/api/api_v1/pets/views.py
+++ fastapi-application/api/api_v1/pets/views.py
@@ -1,11 +1,12 @@
 from typing import Annotated
 
-from fastapi import APIRouter, HTTPException, status, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.models import db_helper
+from core.models import db_helper, Pet
 from . import crud
-from .schemas import PetRead, PetCreate
+from .dependencies import pet_by_id
+from .schemas import PetRead, PetCreate, PetUpdate
 
 router = APIRouter(
     tags=["Pets"],
@@ -16,7 +17,7 @@ router = APIRouter(
 async def get_pets(
     session: Annotated[
         AsyncSession,
-        Depends(db_helper.session_dependency),
+        Depends(db_helper.scoped_session_dependency),
     ],
 ):
     return await crud.read_pets(session=session)
@@ -24,27 +25,63 @@ async def get_pets(
 
 @router.get("/{pet_id}", response_model=PetRead)
 async def get_pet(
-    pet_id: int,
-    session: Annotated[
-        AsyncSession,
-        Depends(db_helper.session_dependency),
+    pet: Annotated[
+        Pet,
+        Depends(pet_by_id),
     ],
 ):
-    pet = await crud.read_pet(session=session, pet_id=pet_id)
-    if pet is not None:
-        return pet
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"Product {pet_id} not found",
-    )
+    return pet
 
 
-@router.post("/", response_model=PetRead)
+@router.post(
+    "/",
+    response_model=PetRead,
+    status_code=status.HTTP_201_CREATED,
+)
 async def create_pet(
     session: Annotated[
         AsyncSession,
-        Depends(db_helper.session_dependency),
+        Depends(db_helper.scoped_session_dependency),
     ],
     pet_create: PetCreate,
 ):
     return await crud.create_pet(session=session, pet_create=pet_create)
+
+
+@router.patch("/{pet_id}", response_model=PetRead)
+async def update_pet(
+    pet_update: PetUpdate,
+    pet: Annotated[
+        Pet,
+        Depends(pet_by_id),
+    ],
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.scoped_session_dependency),
+    ],
+):
+    return await crud.update_pet(
+        session=session,
+        pet=pet,
+        pet_update=pet_update,
+    )
+
+
+@router.delete(
+    "/{pet_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+async def delete_pet(
+    pet: Annotated[
+        Pet,
+        Depends(pet_by_id),
+    ],
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.scoped_session_dependency),
+    ],
+) -> None:
+    await crud.delete_pet(
+        session=session,
+        pet=pet,
+    )
