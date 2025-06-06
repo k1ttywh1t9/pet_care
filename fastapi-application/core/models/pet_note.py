d841a1a42cb619111afc705083b3789ae85f6bd3
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-07 02:00:03 +0300

    create pet note sqla model

---
+++ fastapi-application/core/models/pet_note.py
@@ -0,0 +1,11 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from core.models import Base
+from core.models.mixins import IdIntPkMixin
+from core.types import UserIdType
+
+
+class PetNote(IdIntPkMixin, Base):
+    pet_id: Mapped[int] = mapped_column(ForeignKey("pets.id"))
+    user_id: Mapped[UserIdType] = mapped_column(ForeignKey("user.id"))
