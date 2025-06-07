f92974c5ec8f030b842217545a5a6777dd613d62
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-07 13:07:07 +0300

    add content column for pet note

--- fastapi-application/core/models/pet_note.py
+++ fastapi-application/core/models/pet_note.py
@@ -1,4 +1,6 @@
-from sqlalchemy import ForeignKey
+from datetime import datetime
+
+from sqlalchemy import ForeignKey, Text, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
 
 from core.models import Base
@@ -9,3 +11,5 @@ from core.types import UserIdType
 class PetNote(IdIntPkMixin, Base):
     pet_id: Mapped[int] = mapped_column(ForeignKey("pet.id"))
     user_id: Mapped[UserIdType] = mapped_column(ForeignKey("user.id"))
+
+    content: Mapped[str] = mapped_column(Text)
