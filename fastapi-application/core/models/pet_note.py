7eac9645681274a1b998fd5458b89fc6d0378477
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-07 23:13:08 +0300

    added timestamp mixin to pet_note

--- fastapi-application/core/models/pet_note.py
+++ fastapi-application/core/models/pet_note.py
@@ -4,22 +4,15 @@ from sqlalchemy import ForeignKey, Text, DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from core.models import Base
-from core.models.mixins import IdIntPkMixin
+from core.models.mixins import IdIntPkMixin, TimestampMixin
 from core.types import UserIdType
 
 
-class PetNote(IdIntPkMixin, Base):
+class PetNote(IdIntPkMixin, TimestampMixin, Base):
     pet_id: Mapped[int] = mapped_column(ForeignKey("pet.id"))
     user_id: Mapped[UserIdType] = mapped_column(ForeignKey("user.id"))
 
     content: Mapped[str] = mapped_column(
         Text,
-        nullable=False,
-    )
-    created_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True),
-        default=datetime.utcnow(),
-        server_default=func.now(),
-        onupdate=func.now(),
         nullable=False,
     )
