5beed9382ece07b7e960325e249868c6883c5f88
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-07 13:52:59 +0300

    add params to pet notes crud

--- fastapi-application/api/api_v1/pet_notes/crud.py
+++ fastapi-application/api/api_v1/pet_notes/crud.py
@@ -5,22 +5,45 @@ Update
 Delete
 """
 
+from sqlalchemy.ext.asyncio import AsyncSession
 
-async def create_note():
+from api.api_v1.pet_notes.schemas import PetNoteCreate, PetNoteUpdate
+from core.models import PetNote
+from core.types import UserIdType
+
+
+async def create_note(
+    session: AsyncSession,
+    note_create: PetNoteCreate,
+    user_id: UserIdType,
+) -> PetNote:
     pass
 
 
-async def read_note():
+async def read_note(
+    session: AsyncSession,
+    user_id: UserIdType,
+) -> PetNote | None:
     pass
 
 
-async def read_notes():
+async def read_notes(
+    session: AsyncSession,
+    user_id: UserIdType,
+) -> list[PetNote]:
     pass
 
 
-async def update_note():
+async def update_note(
+    session: AsyncSession,
+    note_update: PetNoteUpdate,
+    user_id: UserIdType,
+) -> PetNote:
     pass
 
 
-async def delete_notes():
+async def delete_notes(
+    session: AsyncSession,
+    user_id: UserIdType,
+) -> None:
     pass
