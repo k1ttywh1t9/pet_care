c990a454af8887fbf3c7725b547d724d1f90bf5e
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-07 23:12:02 +0300

    added timestamp mixin to expense entry

--- fastapi-application/core/models/expence_entry.py
+++ fastapi-application/core/models/expence_entry.py
@@ -1,15 +1,13 @@
-from datetime import datetime
-
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped
 from sqlalchemy.testing.schema import mapped_column
 
 from core.models import Base
-from core.models.mixins import IdIntPkMixin
+from core.models.mixins import IdIntPkMixin, TimestampMixin
 from core.types import UserIdType
 
 
-class ExpenseEntry(IdIntPkMixin, Base):
+class ExpenseEntry(IdIntPkMixin, TimestampMixin, Base):
     pet_id: Mapped[UserIdType] = mapped_column(ForeignKey("pet.id"))
     amount: Mapped[int] = mapped_column()
     purpose: Mapped[str | None] = mapped_column()
