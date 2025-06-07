2e67810bf9f048499a4bdb850dcc89f616e958f0
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-07 13:19:55 +0300

    update pet note sqla model: added created_at column

--- fastapi-application/core/models/pet_note.py
+++ fastapi-application/core/models/pet_note.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import ForeignKey, Text, DateTime
+from sqlalchemy import ForeignKey, Text, DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from core.models import Base
@@ -12,4 +12,14 @@ class PetNote(IdIntPkMixin, Base):
     pet_id: Mapped[int] = mapped_column(ForeignKey("pet.id"))
     user_id: Mapped[UserIdType] = mapped_column(ForeignKey("user.id"))
 
-    content: Mapped[str] = mapped_column(Text)
+    content: Mapped[str] = mapped_column(
+        Text,
+        nullable=False,
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=datetime.utcnow(),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
