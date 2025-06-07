ffe12dcf14e6b7b2552110aa6c7317a480877961
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-07 13:43:18 +0300

    create pet notes crud template

---
+++ fastapi-application/api/api_v1/pet_notes/crud.py
@@ -0,0 +1,26 @@
+"""
+Create
+Read
+Update
+Delete
+"""
+
+
+async def create_note():
+    pass
+
+
+async def read_note():
+    pass
+
+
+async def read_notes():
+    pass
+
+
+async def update_note():
+    pass
+
+
+async def delete_notes():
+    pass
