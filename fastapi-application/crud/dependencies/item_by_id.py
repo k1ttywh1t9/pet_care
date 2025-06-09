8a3c0a65ef1f189afc925adbf21d30677b0959fa
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-09 21:23:38 +0300

    added auth user verifying in crud service

--- fastapi-application/crud/dependencies/item_by_id.py
+++ fastapi-application/crud/dependencies/item_by_id.py
@@ -5,29 +5,32 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 
 from core.models import db_helper
+from core.types import UserIdType
 from crud.service import CRUDService
 
-if TYPE_CHECKING:
-    from sqlalchemy.orm import DeclarativeBase
-
-    class EntityModel(DeclarativeBase):
-        pass
+from .current_active_user import get_current_active_user
 
 
 def get_item_by_id(crud: CRUDService):
     async def dependency(
-        item_id: Annotated[int, Path],
         session: Annotated[
             AsyncSession,
             Depends(db_helper.scoped_session_dependency),
         ],
-    ) -> "EntityModel":
+        user: Annotated[UserIdType, Depends(get_current_active_user)],
+        item_id: Annotated[int, Path],
+    ):
         db_entity = await crud.read_entity(
             session=session,
             entity_id=item_id,
         )
-        if db_entity is not None:
-            return db_entity
+        if db_entity:
+            if db_entity.user_id == user.id:
+                return db_entity
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail=f"{crud.model.__tablename__} entity with id {item_id} do not belong to user {user.id}",
+            )
 
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
