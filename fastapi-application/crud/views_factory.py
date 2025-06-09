32120e25b931874024cf7d68f34f28955824de2b
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-09 21:23:51 +0300

    added auth user verifying in crud views

--- fastapi-application/crud/views_factory.py
+++ fastapi-application/crud/views_factory.py
@@ -1,80 +1,74 @@
-from typing import TYPE_CHECKING, Annotated
+from typing import Annotated, Type, TypeVar, Generic
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Body
 from fastapi.params import Depends
+from pydantic import BaseModel
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import DeclarativeBase
 
 from core.models import db_helper
+from crud.dependencies.current_active_user import get_current_active_user
 from crud.dependencies.item_by_id import get_item_by_id
 from crud.service import CRUDService
 
-if TYPE_CHECKING:
-    from sqlalchemy.orm import DeclarativeBase
-    from pydantic import BaseModel
 
-    class EntityModel(DeclarativeBase):
-        pass
-
-    class EntitySchema(BaseModel):
-        pass
-
-    class EntityCreateSchema(EntitySchema):
-        pass
-
-    class EntityReadSchema(EntitySchema):
-        pass
-
-    class EntityUpdateSchema(EntityCreateSchema):
-        pass
+ORMModel = TypeVar("ORMModel", bound=DeclarativeBase)
+CreateSchema = TypeVar("CreateSchema", bound=BaseModel)
+ReadSchema = TypeVar("ReadSchema", bound=BaseModel)
+UpdateSchema = TypeVar("UpdateSchema", bound=BaseModel)
 
 
-class CRUDViewsFactory:
+class CRUDViewsFactory(Generic[CreateSchema, ReadSchema, UpdateSchema]):
     def __init__(
         self,
-        model: "EntityModel",
+        model: Type[ORMModel],
     ):
         self.model = model
         self.crud: CRUDService = CRUDService(self.model)
 
     def get_router(
         self,
-        create_schema: "EntityCreateSchema",
-        read_schema: "EntityReadSchema",
-        update_schema: "EntityUpdateSchema",
-    ):
+        create_schema: Type[CreateSchema],
+        read_schema: Type[ReadSchema],
+        update_schema: Type[UpdateSchema],
+    ) -> APIRouter:
         router = APIRouter()
 
         crud = self.crud
 
+        create_schema.model_rebuild()
+        read_schema.model_rebuild()
+        update_schema.model_rebuild()
+
+        CreateSchema = create_schema
+        ReadSchema = read_schema
+        UpdateSchema = update_schema
+
         @router.get(
             "/",
-            response_model=list["EntityReadSchema"],
         )
         async def get_entities(
             session: Annotated[
                 AsyncSession,
                 Depends(db_helper.scoped_session_dependency),
             ],
+            user=Depends(get_current_active_user),
         ):
             return await crud.read_entities(
                 session=session,
+                user_id=user.id,
             )
 
         @router.get(
             "/{item_id}",
-            response_model="EntityReadSchema",
         )
         async def get_entity(
-            db_entity: Annotated[
-                "EntityModel",
-                Depends(get_item_by_id(crud=crud)),
-            ],
+            db_entity=Depends(get_item_by_id(crud=crud)),
         ):
             return db_entity
 
         @router.post(
             "/",
-            response_model="EntityReadSchema",
             status_code=status.HTTP_201_CREATED,
         )
         async def create_entity(
@@ -82,27 +76,25 @@ class CRUDViewsFactory:
                 AsyncSession,
                 Depends(db_helper.scoped_session_dependency),
             ],
-            create_schema: "EntityCreateSchema",
+            user=Depends(get_current_active_user),
+            create_schema: CreateSchema = Body(...),
         ):
             return await crud.create_entity(
                 session=session,
+                user_id=user.id,
                 create_schema=create_schema,
             )
 
         @router.patch(
             "/{item_id}",
-            response_model="EntityReadSchema",
         )
         async def update_entity(
             session: Annotated[
                 AsyncSession,
                 Depends(db_helper.scoped_session_dependency),
             ],
-            update_schema: "EntityUpdateSchema",
-            db_entity: Annotated[
-                "EntityModel",
-                Depends(get_item_by_id(crud)),
-            ],
+            update_schema: UpdateSchema = Body(...),
+            db_entity=Depends(get_item_by_id(crud)),
         ):
             return await crud.update_entity(
                 session=session,
@@ -119,7 +111,7 @@ class CRUDViewsFactory:
                 AsyncSession,
                 Depends(db_helper.scoped_session_dependency),
             ],
-            db_entity: Annotated["EntityModel", Depends(get_item_by_id(crud))],
+            db_entity=Depends(get_item_by_id(crud)),
         ):
             await crud.delete_entity(
                 session=session,
