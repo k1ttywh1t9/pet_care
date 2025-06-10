68fb63be58b83c0b35dd6bf515c785af0e978b79
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-10 12:13:55 +0300

    register EE router

--- fastapi-application/api/api_v1/__init__.py
+++ fastapi-application/api/api_v1/__init__.py
@@ -5,6 +5,7 @@ from core.config import settings
 from .users import router as users_router
 from .pets import router as pets_router
 from .pet_notes import router as pet_notes_router
+from .expense_entry import router as expense_entries_router
 
 router = APIRouter(
     prefix=settings.api.v1.prefix,
@@ -23,4 +24,9 @@ router.include_router(
 router.include_router(
     router=pet_notes_router,
     prefix=settings.api.v1.pet_notes,
+)
+
+router.include_router(
+    router=expense_entries_router,
+    prefix=settings.api.v1.expense_entries,
 )
