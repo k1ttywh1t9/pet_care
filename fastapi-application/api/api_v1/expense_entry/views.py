bb6ea0570f85b7d9498e55e83032a33a568456cf
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-10 12:13:33 +0300

    add EE views

---
+++ fastapi-application/api/api_v1/expense_entry/views.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter
+
+from api.api_v1.expense_entry.schemas import (
+    ExpenseEntryCreate,
+    ExpenseEntryRead,
+    ExpenseEntryUpdate,
+)
+from core.models import ExpenseEntry
+from crud import CRUDViewsFactory
+
+router = APIRouter()
+
+
+crud_router = CRUDViewsFactory(
+    model=ExpenseEntry,
+).get_router(
+    create_schema=ExpenseEntryCreate,
+    read_schema=ExpenseEntryRead,
+    update_schema=ExpenseEntryUpdate,
+)
+
+router.include_router(router=crud_router)
