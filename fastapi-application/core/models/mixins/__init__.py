5b00a7848c1227b490b4687d7dd9a8955e1720c8
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-10 10:01:48 +0300

    create user id fk mixin

--- fastapi-application/core/models/mixins/__init__.py
+++ fastapi-application/core/models/mixins/__init__.py
@@ -1,7 +1,9 @@
 __all__ = (
     "IdIntPkMixin",
     "TimestampMixin",
+    "UserIdFkMixin",
 )
 
 from .id_int_pk import IdIntPkMixin
 from .timestamp import TimestampMixin
+from .user_id_fk import UserIdFkMixin
---
+++ fastapi-application/core/models/mixins/user_id_fk.py
@@ -0,0 +1,10 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from core.types import UserIdType
+
+
+class UserIdFkMixin:
+    user_id: Mapped[UserIdType] = mapped_column(
+        ForeignKey("user.id"),
+    )
