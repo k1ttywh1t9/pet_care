8763c280ffa095078838bc30176e240e0be376e5
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-10 10:17:45 +0300

    register expense_entry sqla model

--- fastapi-application/core/models/__init__.py
+++ fastapi-application/core/models/__init__.py
@@ -1,13 +1,8 @@
-__all__ = (
-    "db_helper",
-    "Base",
-    "User",
-    "Pet",
-    "PetNote",
-)
+__all__ = ("db_helper", "Base", "User", "Pet", "PetNote", "ExpenseEntry")
 
 from .db_helper import db_helper
 from .base import Base
 from .user import User
 from .pet import Pet
 from .pet_note import PetNote
+from .expence_entry import ExpenseEntry
