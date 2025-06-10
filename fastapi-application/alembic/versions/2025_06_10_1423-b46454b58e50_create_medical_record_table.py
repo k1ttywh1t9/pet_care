80ea91005f6fd60bc86d22ec21fa6d51e753d3ad
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-10 23:54:50 +0300

    create medical record sqla model migration

---
+++ fastapi-application/alembic/versions/2025_06_10_1423-b46454b58e50_create_medical_record_table.py
@@ -0,0 +1,58 @@
+"""create medical_record table
+
+Revision ID: b46454b58e50
+Revises: 9a73c0970694
+Create Date: 2025-06-10 14:23:48.760842
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "b46454b58e50"
+down_revision: Union[str, None] = "9a73c0970694"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "medical_record",
+        sa.Column("date_performed", sa.Date(), nullable=False),
+        sa.Column("next_due_date", sa.Date(), nullable=True),
+        sa.Column("details", sa.Text(), nullable=False),
+        sa.Column("content", sa.LargeBinary(), nullable=True),
+        sa.Column("is_archived", sa.Boolean(), nullable=False),
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("user_id", sa.Integer(), nullable=False),
+        sa.Column("pet_id", sa.Integer(), nullable=False),
+        sa.Column(
+            "created_at",
+            sa.DateTime(timezone=True),
+            server_default=sa.text("now()"),
+            nullable=False,
+        ),
+        sa.Column(
+            "updated_at",
+            sa.DateTime(timezone=True),
+            server_default=sa.text("now()"),
+            nullable=False,
+        ),
+        sa.ForeignKeyConstraint(
+            ["pet_id"],
+            ["pet.id"],
+        ),
+        sa.ForeignKeyConstraint(
+            ["user_id"],
+            ["user.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("medical_record")
