3a988a1153fc8e073b60f05dbd37294f464927aa
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-19 14:21:25 +0300

    use pet id fk mixin in pet notes sqla model

--- fastapi-application/core/models/pet_note.py
+++ fastapi-application/core/models/pet_note.py
@@ -3,11 +3,10 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 from core.models import Base
 from core.models.mixins import IdIntPkMixin, TimestampMixin, UserIdFkMixin
+from core.models.mixins.pet_id_fk import PetIdFkMixin
 
 
-class PetNote(IdIntPkMixin, UserIdFkMixin, TimestampMixin, Base):
-    pet_id: Mapped[int] = mapped_column(ForeignKey("pet.id"))
-
+class PetNote(IdIntPkMixin, UserIdFkMixin, PetIdFkMixin, TimestampMixin, Base):
     content: Mapped[str] = mapped_column(
         Text,
         nullable=False,
