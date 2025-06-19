e76866cb0f1988cdfbcd4196c7bb9bd3b5f5313c
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-19 22:12:04 +0300

    add relationship to expense entry sqla model

--- fastapi-application/core/models/expense_entry.py
+++ fastapi-application/core/models/expense_entry.py
@@ -1,10 +1,16 @@
-from sqlalchemy.orm import Mapped
+from typing import TYPE_CHECKING
 
-from core.models import Base
-from core.models.mixins import IdIntPkMixin, TimestampMixin, UserIdFkMixin
-from core.models.mixins.pet_id_fk import PetIdFkMixin
+from sqlalchemy.orm import Mapped, relationship
+
+from .base import Base
+from core.models.mixins import IdIntPkMixin, TimestampMixin, UserIdFkMixin, PetIdFkMixin
+
+if TYPE_CHECKING:
+    from .pet import Pet
 
 
 class ExpenseEntry(IdIntPkMixin, UserIdFkMixin, PetIdFkMixin, TimestampMixin, Base):
     amount: Mapped[int]
     purpose: Mapped[str | None]
+
+    pet: Mapped["Pet"] = relationship()
