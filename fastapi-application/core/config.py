08b3ebac94c2d2327c08bdad5314fd952fa6d031
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-19 14:45:18 +0300

    add naming convention for models

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
 BASE_DIR = Path(__file__).parent.parent.parent
 
 
@@ -13,6 +12,14 @@ class DatabaseConfig(BaseModel):
     echo_pool: bool = False
     pool_size: int = 50
     max_overflow: int = 10
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
 
 
 class RunConfig(BaseModel):
