928e130cadd7116becb66930da2147cf2d285869
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-19 22:11:36 +0300

    add relationships to pet sqla model

--- fastapi-application/core/models/pet.py
+++ fastapi-application/core/models/pet.py
@@ -8,9 +8,15 @@ from .mixins import IdIntPkMixin, UserIdFkMixin
 
 if TYPE_CHECKING:
     from .user import User
+    from .pet_note import PetNote
+    from .expense_entry import ExpenseEntry
+    from .medical_record import MedicalRecord
 
 
 class Pet(IdIntPkMixin, UserIdFkMixin, Base):
     name: Mapped[str] = mapped_column(String(60))
 
     owner: Mapped["User"] = relationship(back_populates="pets")
+    notes: Mapped[list["PetNote"]] = relationship()
+    expense_entries: Mapped[list["ExpenseEntry"]] = relationship()
+    medical_records: Mapped[list["MedicalRecord"]] = relationship()
