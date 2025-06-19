a6dab647b97ecc6fe1fa52f88a8738f1918d2dfc
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-19 14:27:30 +0300

    update medical record sqla model

--- fastapi-application/core/models/medical_record.py
+++ fastapi-application/core/models/medical_record.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Date, Text, LargeBinary
+from sqlalchemy import Date, Text, LargeBinary, String
 from sqlalchemy.orm import Mapped
 from datetime import date
 
@@ -10,11 +10,9 @@ from core.models.mixins.pet_id_fk import PetIdFkMixin
 
 
 class MedicalRecord(IdIntPkMixin, UserIdFkMixin, PetIdFkMixin, TimestampMixin, Base):
-    date_performed: Mapped[date] = mapped_column(Date)
-    next_due_date: Mapped[date | None] = mapped_column(Date)
-    details: Mapped[str] = mapped_column(Text)
+    name: Mapped[str] = mapped_column(String(60))
+    
     content: Mapped[bytes] = mapped_column(
         LargeBinary,
         nullable=True,
     )
-    is_archived: Mapped[bool]
