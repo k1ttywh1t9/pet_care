10c572b4c5835f0d4fb792d682d5ce88070dfb0c
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-23 14:50:50 +0300

    add model config from attributes in detail DTOs

--- fastapi-application/api/api_v1/detail_schemas.py
+++ fastapi-application/api/api_v1/detail_schemas.py
@@ -1,3 +1,5 @@
+from pydantic import ConfigDict
+
 from api.api_v1.pets.schemas import PetRead
 from api.api_v1.pet_notes.schemas import PetNoteRead
 from api.api_v1.expense_entries.schemas import ExpenseEntryRead
@@ -9,17 +11,22 @@ class PetDetailsRead(PetRead):
     expense_entries: list[ExpenseEntryRead]
     medical_records: list[MedicalRecordRead]
 
-    class Config:
-        orm_mode = True
+    model_config = ConfigDict(from_attributes=True)
 
 
 class PetNoteDetailsRead(PetNoteRead):
     pet: PetRead
 
+    model_config = ConfigDict(from_attributes=True)
+
 
 class ExpenseEntryDetailsRead(ExpenseEntryRead):
     pet: PetRead
 
+    model_config = ConfigDict(from_attributes=True)
+
 
 class MedicalRecordDetailsRead(MedicalRecordRead):
     pet: PetRead
+
+    model_config = ConfigDict(from_attributes=True)
