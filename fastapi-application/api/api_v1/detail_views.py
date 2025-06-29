ad319a538e5a53db081f3a93f9639ece3d1fd7c9
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-29 19:25:24 +0300

    pin tags to detail views

--- fastapi-application/api/api_v1/detail_views.py
+++ fastapi-application/api/api_v1/detail_views.py
@@ -11,9 +11,7 @@ from core.models import Pet, PetNote, ExpenseEntry, MedicalRecord
 from crud.elements.read_service import ReadService
 from crud_router.elements.read_router import ReadRouterFactory
 
-router = APIRouter(
-    tags=["Detail"],
-)
+router = APIRouter()
 
 read_pet_details_router = ReadRouterFactory(
     model=Pet,
@@ -54,19 +52,23 @@ read_medical_record_details_router = ReadRouterFactory(
 router.include_router(
     router=read_pet_details_router,
     prefix=settings.api.v1.pets + "/detail",
+    tags=["Pets"],
 )
 
 router.include_router(
     router=read_pet_note_details_router,
     prefix=settings.api.v1.pet_notes + "/detail",
+    tags=["Pet notes"],
 )
 
 router.include_router(
     router=read_expense_entry_details_router,
     prefix=settings.api.v1.expense_entries + "/detail",
+    tags=["Expense entries"],
 )
 
 router.include_router(
     router=read_medical_record_details_router,
     prefix=settings.api.v1.medical_records + "/detail",
+    tags=["Medical records"],
 )
