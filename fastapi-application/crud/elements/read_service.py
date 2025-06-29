2685e8242ad17bffc288cf501cb43ca868c8bc81
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-06-29 19:26:15 +0300

    create pagination in crud:read service

--- fastapi-application/crud/elements/read_service.py
+++ fastapi-application/crud/elements/read_service.py
@@ -8,10 +8,10 @@ from crud.elements.base import ServiceBase
 
 class ReadService(ServiceBase):
     async def read_entities(
-            self,
-            session: AsyncSession,
-            user_id: UserIdType,
-            **kwargs,
+        self,
+        session: AsyncSession,
+        user_id: UserIdType,
+        **kwargs,
     ):
         stmt = (
             select(self.model)
@@ -26,19 +26,39 @@ class ReadService(ServiceBase):
         entities = list(result.scalars().all())
         return entities
 
+    async def read_paginated_entities(
+        self,
+        session: AsyncSession,
+        user_id: UserIdType,
+        offset: int = 0,
+        limit: int = 6,
+        **kwargs,
+    ):
+        stmt = (
+            select(self.model)
+            .where(self.model.user_id == user_id)
+            .offset(offset=offset)
+            .limit(limit=limit)
+            .order_by(self.model.id)
+        )
+
+        for rel in self.model.load_relations:
+            stmt = stmt.options(selectinload(getattr(self.model, rel)))
+
+        result: Result = await session.execute(statement=stmt)
+        entities = list(result.scalars().all())
+        return entities
+
     async def read_entity(
-            self,
-            session: AsyncSession,
-            entity_id: int,
-            **kwargs,
+        self,
+        session: AsyncSession,
+        entity_id: int,
+        **kwargs,
     ):
         # db_entity = await session.get(self.model, entity_id)
         # return db_entity
 
-        stmt = (
-            select(self.model)
-            .where(self.model.id == entity_id)
-        )
+        stmt = select(self.model).where(self.model.id == entity_id)
 
         for rel in self.model.load_relations:
             stmt = stmt.options(selectinload(getattr(self.model, rel)))
