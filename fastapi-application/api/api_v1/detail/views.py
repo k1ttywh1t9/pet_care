9720da54d1ee979509d5e15e95c6ebbc9a9d79a8
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-07-05 04:48:54 +0300

    move detail api to its own package

--- fastapi-application/api/api_v1/detail/views.py
+++ fastapi-application/api/api_v1/detail/views.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from api.api_v1.detail_schemas import (
+from api.api_v1.detail.schemas import (
     PetDetailsRead,
     PetNoteDetailsRead,
     ExpenseEntryDetailsRead,
