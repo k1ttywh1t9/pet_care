4938c76232ad230de3e4261e8fbc4030ae8e2433
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-07-06 07:25:52 +0300

    create upload views

---
+++ fastapi-application/api/api_v1/upload/views.py
@@ -0,0 +1,46 @@
+from typing import Annotated
+
+from fastapi import APIRouter, UploadFile, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from api.api_v1.pets.schemas import PetUpdate
+from core.models import Pet, db_helper
+from crud.dependencies.item_by_id import get_item_by_id
+from crud.elements.read_service import ReadService
+from crud.elements.update_service import UpdateService
+
+from s3.client import client
+
+router = APIRouter()
+
+
+update_pet = UpdateService(
+    model=Pet,
+).update_entity
+
+read_pet = ReadService(
+    model=Pet,
+)
+
+
+@router.post("/set-image/pet/{item_id}")
+async def set_pet_image(
+    session: Annotated[AsyncSession, Depends(db_helper.scoped_session_dependency)],
+    pet: Annotated[Pet, Depends(get_item_by_id(read_pet))],
+    image: UploadFile,
+):
+
+    image_url = await client.upload_file(
+        filename=image.filename,
+        file=await image.read(),
+    )
+
+    update_schema = PetUpdate(
+        image_url=image_url,
+    )
+
+    return await update_pet(
+        session=session,
+        entity=pet,
+        update_schema=update_schema,
+    )
