cd742e8b18feec7c9d17901690556436bb370671
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-07-07 11:29:23 +0300

    use update service dependency in update router factory

--- fastapi-application/crud_router/elements/update_router.py
+++ fastapi-application/crud_router/elements/update_router.py
@@ -4,9 +4,10 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import db_helper
+from crud.dependencies.get_service_dependencies import get_update_service_dependency
 from crud.dependencies.item_by_id import get_item_by_id
 from crud_router.elements.base import FactoryBase
-from crud_router.elements.types import UpdateSchema, ReadSchema, ORMModel
+from crud_router.elements.types import UpdateSchema, ReadSchema, ORMModel, ORMService
 
 
 class UpdateRouterFactory(FactoryBase):
@@ -14,16 +15,16 @@ class UpdateRouterFactory(FactoryBase):
         self,
         update_schema: Type[UpdateSchema],
         read_schema: Type[ReadSchema],
-    ):
+    ) -> APIRouter:
         router = APIRouter()
-
-        service = self.service
 
         update_schema.model_rebuild()
         read_schema.model_rebuild()
 
         UpdateSchema = update_schema
         ReadSchema = read_schema
+
+        model = self.model
 
         @router.patch(
             "/{item_id}",
@@ -33,10 +34,14 @@ class UpdateRouterFactory(FactoryBase):
             session: Annotated[
                 AsyncSession, Depends(db_helper.scoped_session_dependency)
             ],
+            service: Annotated[
+                ORMService,
+                Depends(get_update_service_dependency(model)),
+            ],
             update_schema: UpdateSchema,
             entity: Annotated[
                 Type[ORMModel],
-                Depends(get_item_by_id(service)),
+                Depends(get_item_by_id(model)),
             ],
         ):
             return await service.update_entity(
