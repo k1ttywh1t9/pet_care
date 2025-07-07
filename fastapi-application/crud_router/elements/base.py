17b7f87053a6787a9e70a68d2f9e12c8388671a3
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-07-07 11:28:12 +0300

    remove service parameter in router factory base init

--- fastapi-application/crud_router/elements/base.py
+++ fastapi-application/crud_router/elements/base.py
@@ -1,13 +1,11 @@
 from typing import Type
 
-from crud_router.elements.types import ORMModel, ORMService
+from crud_router.elements.types import ORMModel
 
 
 class FactoryBase:
     def __init__(
         self,
         model: Type[ORMModel],
-        service: ORMService,
     ):
         self.model = model
-        self.service = service
