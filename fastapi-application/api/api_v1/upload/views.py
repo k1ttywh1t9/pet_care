4f990f40dd0f537c190f3acd6a28a2d4d004e037
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-07-10 12:03:25 +0300

    register pet image handler

--- fastapi-application/api/api_v1/upload/views.py
+++ fastapi-application/api/api_v1/upload/views.py
@@ -1,40 +1,8 @@
-from typing import Annotated
+from fastapi import APIRouter
 
-from fastapi import APIRouter, UploadFile, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
+from .pet_image_handler import router as pet_image_router
 
-from api.api_v1.pets.schemas import PetAvatarUpdate
-from core.models import Pet, db_helper
-from crud.dependencies.item_by_id import get_item_by_id
-from crud.elements.update_service import UpdateService
-from s3.client import client
 
 router = APIRouter()
 
-
-update_pet = UpdateService(
-    model=Pet,
-).update_entity
-
-
-@router.post("/set-image/pet/{item_id}")
-async def set_pet_image(
-    session: Annotated[AsyncSession, Depends(db_helper.scoped_session_dependency)],
-    pet: Annotated[Pet, Depends(get_item_by_id(Pet))],
-    image: UploadFile,
-):
-
-    image_url = await client.upload_file(
-        filename=image.filename,
-        file=await image.read(),
-    )
-
-    update_schema = PetAvatarUpdate(
-        image_url=image_url,
-    )
-
-    return await update_pet(
-        session=session,
-        entity=pet,
-        update_schema=update_schema,
-    )
+router.include_router(pet_image_router)
