1c3bf8b923c1208df07dd7ceb890b8f81ff74805
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-07-10 09:06:19 +0300

    add validation to pet id fields via mixin

---
+++ fastapi-application/api/api_v1/mixins/pet_id_mixin.py
@@ -0,0 +1,11 @@
+from typing import Annotated, Optional
+
+from pydantic import Field
+
+
+class PetIdFieldMixin:
+    pet_id: Annotated[int, Field(gt=0)]
+
+
+class PetIdOptionalFieldMixin:
+    pet_id: Annotated[Optional[int], Field(gt=0)]
