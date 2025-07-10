6c697b86a1178870eef18aae58cf9774f7e9d406
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-07-10 09:19:06 +0300

    make avatar optional

--- fastapi-application/api/api_v1/pets/schemas.py
+++ fastapi-application/api/api_v1/pets/schemas.py
@@ -7,7 +7,7 @@ from api.api_v1.mixins import IdFieldMixin, UserIdFieldMixin
 
 class PetBase(BaseModel):
     name: Annotated[str, Field(max_length=35)]
-    image_url: Annotated[HttpUrl, Field()]
+    image_url: Annotated[Optional[HttpUrl], Field()]
 
 
 class PetRead(IdFieldMixin, UserIdFieldMixin, PetBase):
