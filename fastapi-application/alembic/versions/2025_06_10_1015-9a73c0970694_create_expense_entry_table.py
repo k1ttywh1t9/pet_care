dce456e52080eb84bb328bea626f1cc4a2a02459
k1ttywh1t9 <k1ttywh1t9@gmail.com>
2025-07-12 17:07:47 +0300

    expense entry: add constraint names for pet id fk + user id fk

--- fastapi-application/alembic/versions/2025_06_10_1015-9a73c0970694_create_expense_entry_table.py
+++ fastapi-application/alembic/versions/2025_06_10_1015-9a73c0970694_create_expense_entry_table.py
@@ -42,10 +42,12 @@ def upgrade() -> None:
         sa.ForeignKeyConstraint(
             ["pet_id"],
             ["pet.id"],
+            "expense_entry_pet_id_fkey",
         ),
         sa.ForeignKeyConstraint(
             ["user_id"],
             ["user.id"],
+            "expense_entry_user_id_fkey",
         ),
         sa.PrimaryKeyConstraint("id"),
     )
